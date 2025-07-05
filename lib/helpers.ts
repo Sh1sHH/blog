514b7170d38e00ae829e8e0c716a70503aa77509
Sh1sHH <yyusufunal997@gmail.com>
2025-07-05 03:46:13 +0300

    test deneme

---
+++ lib/helpers.ts
@@ -0,0 +1,11 @@
+// lib/helpers.ts
+
+export const createSlug = (title: string): string => {
+    return title
+      .toLowerCase()
+      .replace(/[^a-z0-9\s-]/g, '')
+      .replace(/\s+/g, '-')
+      .replace(/-+/g, '-')
+      .trim()
+      .replace(/^-+|-+$/g, '');
+  };
